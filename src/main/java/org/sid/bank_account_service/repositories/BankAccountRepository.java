25c0ff33a83348bf753e928f7f4dc56d4c8b8097
yassine-fanidi <yassinefanidi55@gmail.com>
2026-09-22 13:53:10 +0000

    Spring data rest & Test

--- src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
@@ -1,7 +1,13 @@
 package org.sid.bank_account_service.repositories;
 
 import org.sid.bank_account_service.entities.BankAccount;
+import org.sid.bank_account_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    List<BankAccount> findByType(AccountType type);
 }
