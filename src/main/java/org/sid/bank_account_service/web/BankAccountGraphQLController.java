7c72cb30ea2774cf47816ae3aabe60343fe68f92
yassine-fanidi <yassinefanidi55@gmail.com>
2026-09-25 19:18:10 +0000

    Création du fichier schema.graphqls & BankAccoutGraphQlController

---
+++ src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
@@ -0,0 +1,20 @@
+package org.sid.bank_account_service.web;
+
+import org.sid.bank_account_service.entities.BankAccount;
+import org.sid.bank_account_service.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+
+    @QueryMapping
+    public List<BankAccount> accountsList() {
+        return bankAccountRepository.findAll();
+    }
+}
